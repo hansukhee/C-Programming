f0f4b3d5dd11436af156c2e3a37402bda34db6a6
hansukhee <125713702+hansukhee@users.noreply.github.com>
2023-07-11 04:39:42 +0900

    Add files via upload

---
+++ 20230711_array/array-3.cpp
@@ -0,0 +1,59 @@
+// 한라대학교 미래모빌리티공학과 202315021 한석희
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+/*void main()
+{
+	printf("\n");
+
+	int score[10] = { 100,95,85,77,66,55,67,78,89,100 };
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", score[a]);
+	}
+	printf("\n");
+}*/
+
+/*void main()
+{
+	int score[6];
+	int i, a;
+	int sum = 0;
+
+	for (i = 0; i < 6; i++)
+	{
+		printf("학생 %d 성적을 입력하시오: ", i + 1);
+		scanf("%d", score[i]);
+	}
+
+	for (i = 0; i < 6; i++)
+	{
+		sum += score[i];
+	}
+
+	printf("합계: %d\n", sum);
+	printf("평균: %.2lf\n", (double)sum / 6);
+	printf("\n score[6]에 저장되어있는 숫자: ");
+
+	for (a = 0; a < 6; a++)
+	{
+		printf("%d", score[a]);
+	}
+
+}*/
+
+/*void main()
+{
+	char string1[6] = { 'H','E','I','I','O' };
+	char string2[6] = "World";
+	char string3[10];
+
+	printf("이름을 입력하시오: ");
+	scanf_s("%s", &string1);
+
+	printf("%s\n", string1);
+	printf("%s\n", string2);
+	printf("%s %s %s님 안녕하세요 \n", string1, string2, string3);
+}*/
