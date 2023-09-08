ef928f82054455dec1b2ece9817e25bb1a285010
hansukhee <125713702+hansukhee@users.noreply.github.com>
2023-09-08 14:27:08 +0900

    Add files via upload

---
+++ 2학기/20230908/20230908/20230908.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+// 20230908-1
+/*void main()
+{
+	int num = 123;
+	int* ptr = &num;
+
+	printf("%d\n", *ptr);	// 주소가 가르키는 값(내용) 출력
+	// printf("%d\n", ptr);	// 주솟값 출력
+}*/
+
+
+// 20230908-2
+/*void main()
+{
+	int num = 123;
+
+	printf("%d\n", num);
+
+};*/
+
+// 20230908-3
+/*void main()
+{
+		int num = 123;
+		int* ptr = &num;
+		//int *ptr = &num;
+		//int * ptr = &num;
+
+		return 0;
+};*/
