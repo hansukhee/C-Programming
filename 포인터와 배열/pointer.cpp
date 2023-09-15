36a6e3bc17917b5bb510949da4f38267adc12946
hansukhee <125713702+hansukhee@users.noreply.github.com>
2023-09-15 15:10:11 +0900

    Add files via upload

---
+++ 포인터와 배열/pointer.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+/*int main(void)
+{
+	char a[8];
+	char* ptr;
+
+	a[0] = 'a';
+	a[1] = 'b';
+	a[2] = 'c';
+	a[3] = 'd';
+	a[4] = 'e';
+	a[5] = 'f';
+	a[6] = 'g';
+	a[7] = '\0';
+
+	ptr = a;
+	printf("%c\n", *ptr);
+
+	ptr = ptr + 3;
+	printf("%c\n", *ptr);
+
+}*/
+
+/*int mian(void)
+{
+	char a[8];
+	char* ptr;
+	int i;
+
+	a[0] = 'a';
+	a[1] = 'b';
+	a[2] = 'c';
+	a[3] = 'd';
+	a[4] = 'e';
+	a[5] = 'f';
+	a[6] = 'g';
+	a[7] = '\0';
+
+	ptr = a;
+	*ptr = 'Z';
+
+	ptr += 3;
+	*ptr = 'W';
+
+	printf("%s\n", a);
+}*/
+
+/*int main(void)
+{
+	int a = 1;
+	int* ptr = &a;
+
+	*ptr = 128;
+
+	printf("%d\n", a);
+}*/
+
+/*void func()
+{
+	printf("function\n");
+}
+
+int mian()
+{
+	void (*ptr)();
+
+	ptr = &func;
+
+	(*ptr)();
+
+	return 0;
+}*/
