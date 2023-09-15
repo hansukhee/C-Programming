13bf909ccf6bdef6c4d8a2448a770f9f071c5ea3
hansukhee <125713702+hansukhee@users.noreply.github.com>
2023-09-15 14:45:34 +0900

    Add files via upload

---
+++ 포인터와 함수 차이점/20230915.cpp
@@ -0,0 +1,88 @@
+#include <stdio.h>
+
+/*void ChangePlusOne(int n)
+{
+	n += 1;
+}
+int main(void)
+{
+	int number = 3;
+	printf("%d\n", number);
+
+	number = 5;
+	printf("%d\n", number);
+	
+	ChangePlusOne(number);
+	printf("%d\n", number);
+}*/
+
+/*void ChangePlusOne(int* n)
+{
+	*n += 1;
+}
+int main(void)
+{
+	int number = 3;
+	printf("%d\n", number);
+
+	number = 5;
+	printf("%d\n", number);
+
+	ChangePlusOne(&number);
+	printf("%d\n", number);
+}*/
+
+/*int main(void)
+{
+	int num1 = 3;
+	int num2 = 6;
+	int* num1pointer = &num1;
+	int* num2pointer = &num2;
+
+	printf("%d, %d\n", num1, *num1pointer);
+	printf("%d, %d\n", num2, *num2pointer);
+}*/
+
+/*int main(void)
+{
+	int num = 5;
+	int* p;
+	p = &num;
+
+	printf(" num: %d\n", num);
+	printf("  *p: %d\n", *p);
+	printf("   p: %d\n", p);
+	printf("&num: %d\n", &num);
+
+}*/
+
+/*void ChangeDouble(int* p)
+{
+	*p = *p * 2;
+}
+int main(void)
+{
+	int num;
+
+	printf("숫자 입력: ");
+	scanf_s("%d", &num);
+
+	ChangeDouble(&num);
+	printf("%d\n", num);
+}*/
+
+/*void ChangeDouble(int* p)
+{
+	*p = *p * 2;
+}
+int main(void)
+{
+	int num;
+
+	printf("숫자 입력: ");
+	scanf_s("%d", &num);
+
+	ChangeDouble(&num);
+	printf("%d\n", num);
+}*/
+
