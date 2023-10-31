35f3549ce48189f879ca5ddad213a0352a53692d
hansukhee <125713702+hansukhee@users.noreply.github.com>
2023-11-01 02:11:33 +0900

    Add files via upload

---
+++ 2023-2학기/코딩 도장꺠기/20231101/20231101.cpp
@@ -0,0 +1,121 @@
+#include <stdio.h>
+
+/*int main()
+{
+    int num1 = 10;
+
+    printf("%d\n", num1 == 10);
+    printf("%d\n", num1 != 5);
+
+    printf("%d\n", num1 > 10);
+    printf("%d\n", num1 < 10);
+
+    printf("%d\n", num1 >= 10);
+    printf("%d\n", num1 <= 10);
+
+    return 0;
+}*/
+
+/*int main()
+{
+    int num1 = 5;
+    int num2;
+
+    if (num1)    
+        num2 = 100;   
+    else              
+        num2 = 200;    
+
+    printf("%d\n", num2); 
+}*/
+
+/*int main()
+{
+    int num1 = 5;
+    int num2;
+
+    num2 = num1 ? 100 : 200;  
+
+    printf("%d\n", num2);  
+    return 0;
+}*/
+
+/*int main()
+{
+    int num1 = 10;
+
+    if (num1 == 10)  
+        printf("10입니다.\n");
+
+    if (num1 != 5)   
+        printf("5가 아닙니다.\n");
+
+    if (num1 > 10)   
+        printf("10보다 큽니다.\n");
+
+    if (num1 < 10)     
+        printf("10보다 작습니다.\n");
+
+    if (num1 >= 10)   
+        printf("10보다 크거나 같습니다.\n");
+
+    if (num1 <= 10)   
+        printf("10보다 작거나 같습니다.\n");
+
+    return 0;
+}*/
+
+/*int main()
+{
+    float num1 = 0.1f;
+    char c1 = 'a';
+
+    if (num1 >= 0.09f) 
+        printf("0.09보다 크거나 같습니다.\n");
+
+    if (c1 == 'a')     
+        printf("a입니다.\n");
+
+    if (c1 == 97)     
+        printf("97입니다.\n");
+
+    if (c1 < 'b')       
+        printf("b보다 작습니다.\n");
+
+    return 0;
+}*/
+
+/*int main()
+{
+    int num1 = 5;
+
+   
+    printf("%s\n", num1 == 10 ? "10입니다." : "10이 아닙니다.");
+
+    return 0;
+}*/
+
+/*int main()
+{
+    int num1 = 27;
+
+    printf("%d\n", num1 > 10);
+    printf("%d\n", num1 != 5);
+
+    printf("%d\n", num1 >= 27);
+    printf("%d\n", num1 == 27);
+
+    printf("%d\n", num1 < 30);
+    printf("%d\n", num1 <= 27);
+
+    return 0;
+}*/
+
+/*int main()
+{
+    float num1 = 1.2f;
+
+    printf("%f\n", num1 > 2.0f >= 3.0f == 4.0f);
+
+    return 0;
+}*/
